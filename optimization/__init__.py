317ae38b23d24b333f94246dcda2801af601da0c
Laurin <laurin.sefa@student.uni-tuebingen.de>
2025-12-15 03:07:00 +0100

    Removed optimization directory

--- optimization/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Optimization package (deprecated).
-
-This module is deprecated. All functionality has been moved to the dfbench package.
-Please use 'from dfbench import ...' instead of 'from optimization import ...'.
-
-This file is kept empty for reference only.
-"""
