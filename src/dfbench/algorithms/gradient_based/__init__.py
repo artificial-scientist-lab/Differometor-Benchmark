6b0fabb5e5482ab3ce8a39204d40f58a1ec2041e
Laurin <laurin.sefa@student.uni-tuebingen.de>
2026-03-30 00:00:17 +0200

    Register Optax algorithms in gradient_based __init__

--- src/dfbench/algorithms/gradient_based/__init__.py
+++ src/dfbench/algorithms/gradient_based/__init__.py
@@ -4,8 +4,76 @@ from dfbench.algorithms.gradient_based.adam_gd import AdamGD
 from dfbench.algorithms.gradient_based.na_adam_gd import NAAdamGD
 from dfbench.algorithms.gradient_based.sa_gd import SAGD
 
+# Optax batch
+from dfbench.algorithms.gradient_based.optax_adam import OptaxAdam
+from dfbench.algorithms.gradient_based.optax_adamw import OptaxAdamW
+from dfbench.algorithms.gradient_based.optax_adabelief import OptaxAdaBelief
+from dfbench.algorithms.gradient_based.optax_adafactor import OptaxAdafactor
+from dfbench.algorithms.gradient_based.optax_amsgrad import OptaxAMSGrad
+from dfbench.algorithms.gradient_based.optax_adagrad import OptaxAdaGrad
+from dfbench.algorithms.gradient_based.optax_adadelta import OptaxAdaDelta
+from dfbench.algorithms.gradient_based.optax_adamax import OptaxAdaMax
+from dfbench.algorithms.gradient_based.optax_adamaxw import OptaxAdaMaxW
+from dfbench.algorithms.gradient_based.optax_adan import OptaxAdan
+from dfbench.algorithms.gradient_based.optax_lion import OptaxLion
+from dfbench.algorithms.gradient_based.optax_lamb import OptaxLAMB
+from dfbench.algorithms.gradient_based.optax_nadam import OptaxNadam
+from dfbench.algorithms.gradient_based.optax_nadamw import OptaxNadamW
+from dfbench.algorithms.gradient_based.optax_rmsprop import OptaxRMSProp
+from dfbench.algorithms.gradient_based.optax_rprop import OptaxRProp
+from dfbench.algorithms.gradient_based.optax_radam import OptaxRAdam
+from dfbench.algorithms.gradient_based.optax_sgd import OptaxSGD, OptaxSGDM, OptaxNAG
+from dfbench.algorithms.gradient_based.optax_noisy_sgd import OptaxNoisySGD
+from dfbench.algorithms.gradient_based.optax_polyak_sgd import OptaxPolyakSGD
+from dfbench.algorithms.gradient_based.optax_sam import OptaxSAM
+from dfbench.algorithms.gradient_based.optax_sophia import OptaxSophia
+from dfbench.algorithms.gradient_based.optax_lookahead import OptaxLookahead
+from dfbench.algorithms.gradient_based.optax_schedule_free_adam import OptaxScheduleFreeAdam
+from dfbench.algorithms.gradient_based.optax_yogi import OptaxYogi
+from dfbench.algorithms.gradient_based.optax_novograd import OptaxNovoGrad
+from dfbench.algorithms.gradient_based.optax_ogd import OptaxOGD
+from dfbench.algorithms.gradient_based.optax_oadam import OptaxOAdam
+from dfbench.algorithms.gradient_based.optax_sign import OptaxSignSGD, OptaxSignum
+from dfbench.algorithms.gradient_based.optax_sm3 import OptaxSM3
+from dfbench.algorithms.gradient_based.optax_lbfgs import OptaxLBFGS
+
 __all__ = [
     "AdamGD",
     "NAAdamGD",
     "SAGD",
+    # Optax batch
+    "OptaxAdam",
+    "OptaxAdamW",
+    "OptaxAdaBelief",
+    "OptaxAdafactor",
+    "OptaxAMSGrad",
+    "OptaxAdaGrad",
+    "OptaxAdaDelta",
+    "OptaxAdaMax",
+    "OptaxAdaMaxW",
+    "OptaxAdan",
+    "OptaxLion",
+    "OptaxLAMB",
+    "OptaxNadam",
+    "OptaxNadamW",
+    "OptaxRMSProp",
+    "OptaxRProp",
+    "OptaxRAdam",
+    "OptaxSGD",
+    "OptaxSGDM",
+    "OptaxNAG",
+    "OptaxNoisySGD",
+    "OptaxPolyakSGD",
+    "OptaxSAM",
+    "OptaxSophia",
+    "OptaxLookahead",
+    "OptaxScheduleFreeAdam",
+    "OptaxYogi",
+    "OptaxNovoGrad",
+    "OptaxOGD",
+    "OptaxOAdam",
+    "OptaxSignSGD",
+    "OptaxSignum",
+    "OptaxSM3",
+    "OptaxLBFGS",
 ]
